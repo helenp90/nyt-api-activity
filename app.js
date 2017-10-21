4b2793e80e6b03039d537be3e6d9ad23aa8231b3
Justin Rice <jsrice7391@gmail.com>
2017-10-21 15:07:36 -0400

    Headlines, picture, and subtext work, the API works, but I am looking into the 404 error

--- app.js
+++ app.js
@@ -6,45 +6,57 @@ var startYear;
 var endYear;
 
 
-$("#search").on("click", function() {
+$("#search").on("click", get_all_the_articles);
+
+
+
+
+
+function get_all_the_articles() {
     searchTerm = $("#searchTerm").val();
     numRecords = $("#numRecords").val();
     startYear = $("#startYear").val();
     endYear = $("#endYear").val();
-});
 
-
-
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-    // 'api-key': "911070957e70436e8b86d093a0848275",
-    // 'q': searchTerm,
-    // 'begin_date': startYear,
-    // 'end_date': endYear,
-    // 'sort': "newest",
-    // 'page': 1
-    'api-key': "911070957e70436e8b86d093a0848275",
-    'q': "patriots"
-});
-$.ajax({
-    url: url,
-    method: 'GET',
-}).done(function(response) {
-    var article_div = $("<div>");
-    var articles = response.response.docs;
-
-
-    articles.forEach(function(article) {
-        var art_div = $("<div>");
-        console.log(article.headline.main);
-        art_div.append("<h1>" + article.headline.main + "</h1>");
-        art_div.append("<p>" + article.snippet + "</p>");
-        $("#results").prepend(art_div);
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'q': searchTerm,
+        // 'begin_date': 20171017,
+        // 'end_date': 20171010,
+        'sort': "newest",
+        'page': 1,
+        'api-key': "911070957e70436e8b86d093a0848275",
 
     });
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(response) {
+        var article_div = $("<div>");
+        var articles = response.response.docs;
+
+
+        articles.forEach(function(article) {
+            console.log(article);
+            var art_div = $('<div class="card" style="width: 20rem;">').css("float", "left");
+            var art_header = $("<img class='card-img-top'>");
+            art_header.attr({
+                src: "http://www.nyt.com/" + article.multimedia[1].url,
+            })
+            console.log(article.multimedia[1].url);
+            art_div.append(art_header)
+            art_div.append("<h4 class='card-title'><a href='" + article.web_url + "'>" + article.headline.main + " </h4>");
+
+            art_div.append("<p class='card-text'>" + article.snippet + "</p>");
+
+            $("#results").prepend(art_div);
+
+        });
 
 
 
-}).fail(function(err) {
-    throw err;
-});
+    }).fail(function(err) {
+        throw err;
+    })
+
+};
